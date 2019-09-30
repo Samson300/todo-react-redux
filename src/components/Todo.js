777288006aea7fc956cebd11af75588755a3454e
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-09-29 22:15:36 -0400

    adds input and button to Todo component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,7 +3,8 @@ import React from "react";
 export default function Todo() {
     return (
         <div>
-            
+            <input id="todo" type="text"></input>
+            <button id="submit-todo">submit</button>
         </div>
     )
 }
